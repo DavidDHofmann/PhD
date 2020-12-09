bc94a1079bbf5b35e312d9ade3f5ef31b5506e2e
davidhofmann92 <davidhofmann92@hotmail.com>
2020-12-09 13:28:47 +0100

    Various updates

--- Chapter_1/02_R-Scripts/02_Analysis/01_SSF(CovariateExtraction).R
+++ Chapter_1/02_R-Scripts/02_Analysis/01_SSF(CovariateExtraction).R
@@ -17,10 +17,20 @@ library(tidyverse)    # For data wrangling
 library(davidoff)     # Custom functions
 library(lubridate)    # To handle dates
 library(amt)          # To coerce gps fixes to steps
+library(raster)       # To handle spatial data
+library(rgdal)        # To handle spatial data
+library(pbmcapply)    # For multicore abilities
+
+# Specify if you're going for iSSF or TiSSF
+# method <- "iSSF"
+method <- "TiSSF"
 
 # Set seed for reproducability
 set.seed(1234)
 
+################################################################################
+#### Data Cleaning
+################################################################################
 # Load the gps data
 data <- read_csv("03_Data/02_CleanData/00_General_Dispersers_POPECOL.csv")
 
@@ -33,6 +43,13 @@ data$TimestampRounded <- round_date(data$Timestamp, "1 hour")
 # Make sure there are no duplicates!
 table(duplicated(data[, c("DogName", "TimestampRounded")]))
 table(hour(data$TimestampRounded))
+
+# We either use iSSF or TiSSF. Hence, we may need to make the data regular. For
+# this, we will only be using fixes that were collected during the following
+# hours: 3, 7, 15, 19, 23
+if (method == "iSSF"){
+  data <- subset(data, hour(TimestampRounded) %in% c(3, 7, 15, 19, 23))
+}
 
 # We want to convert the fixes to steps. However, we don't want to consider any
 # steps that take longer than 8.25 hours. Let's therefore identify consecutive
@@ -58,7 +75,10 @@ data <- data %>%
   subset(Nrow >= 3) %>%
   unnest(data) %>%
   ungroup() %>%
-  select(-c(Nrow, dt_))
+  dplyr::select(-c(Nrow, dt_))
+
+# Create bursts that are unique
+data$BurstID <- data %>% group_indices(DogName, BurstID)
 
 # Now we can coerce the data to proper steps. Note that we pretend that the
 # burst is the animal.
@@ -107,323 +127,102 @@ tracks <- data %>%
   # of any steps where the turning angle is NA
   filter(!is.na(ta_))
 
-# Reproject coordinates
-tracks[, c("x1_", "y1_")] <- reprojCoords(
-    xy    = cbind(tracks$x1_, tracks$y1_)
-  , from  = CRS("+init=epsg:32734")
-  , to    = CRS("+init=epsg:4326")
-)
-tracks[, c("x2_", "y2_")] <- reprojCoords(
-    xy    = cbind(tracks$x2_, tracks$y2_)
-  , from  = CRS("+init=epsg:32734")
-  , to    = CRS("+init=epsg:4326")
+# Generate step id and indicate if case or control step
+tracks <- tracks %>% mutate(
+    step_id_ = 1:nrow(.)
+  , case_    = 1
 )
 
-# Function to reproject coordinates
-reprojCoords <- function(xy, from, to){
-  xy <- as.data.frame(xy)
-  coordinates(xy) <- as.matrix(xy)
-  crs(xy) <- from
-  reproj <- spTransform(xy, to)
-  reproj <- coordinates(reproj)
-  reproj <- as.matrix(reproj)
-  reproj <- unname(reproj)
-  return(reproj)
-}
-
-test <- lineTrack(track = tracks, crs = CRS("+init=epsg:4326"))
-
-library(rgdal)
-writeOGR(test, ".", "test", driver = "ESRI Shapefile")
-plot(test)
-
 ################################################################################
-#### LEGACYY
-################################################################################
-################################################################################
-#### WORK HERE!!!
-################################################################################
-################################################################################
-#### Data Cleaning
-################################################################################
-# For this part we will use the GPS fixes from dispersers only
-tracks <- data %>%
-
-  # Subset to the dispersers only (4 hourly fixes are actually only available
-  # during dispersal)
-  filter(., State == "Disperser") %>%
-
-  # Ultimately, we will only be using fixes that were collected during the
-  # following hours: 3, 7, 15, 19, 23 (rounded to 15 minutes)
-  filter(.
-    , strftime(round_date(Timestamp, "30 minutes")
-      , tz      = "UTC"
-      , format  = "%H:%M:%S") %in%
-      c("03:00:00", "07:00:00", "15:00:00", "19:00:00", "23:00:00")) %>%
-
-  # We also remove popecols fixes before october 2016 because the fixrate scheme
-  # was slightly different before that. We still want to conserve the fixes from
-  # Abrahms' dispersers
-  filter(.,
-    Timestamp >= as.POSIXct('2016-10-01 10:00:00') |
-    DogName %in% c("Lupe", "Scorpion", "Stetson")
-  ) %>%
-
-  # Using the subsetted GPS fixes we prepare tracks
-  make_track(.
-    , .x    = x
-    , .y    = y
-    , .t    = Timestamp
-    , id    = DogName
-    , crs   = CRS("+init=epsg:4326")
-    , State = State
-  ) %>%
-
-  # Transform the tracks to utm
-  transform_coords(CRS("+init=epsg:32734")) %>%
-
-  # Add a column that indicates the time of the day. I follow Gabrieles paper
-  # and night starts when the sun is 18 degree below the horizion
-  time_of_day(., solar.dep = 18, include.crepuscule = FALSE) %>%
-
-  # Nest the tracks
-  nest(data = -"id") %>%
-
-  # Add column that indicates number of fixes
-  mutate(NoFixes = data %>%
-    lapply(nrow) %>%
-    do.call(rbind, .) %>%
-    as.vector()) %>%
-
-  # Keep only those individuals where we have more than 3 fixes during disp.
-  subset(NoFixes > 3) %>%
-
-  # Turn to a step representation (look up the amt vignette for details)
-  mutate(data = map(data, function(x){
-    x %>%
-
-      # The function creates steps from the fixes in each tibble row. The option
-      # "keep_cols" allows to keep the time of day (tod) column that we added
-      steps(keep_cols = "start") %>%
-
-      # Transform the difftime column to a numeric column to avoid that there are
-      # heterogeneous units
-      mutate(., dt_ = as.numeric(dt_, units = "hours")) %>%
-
-      # We get rid of any steps that take longer than 4 hours, unless they
-      # started at 7:00.
-      filter(., strftime(
-          round_date(t1_, "30 minutes")
-        , tz = "UTC"
-        , format = "%H:%M:%S") == "07:00:00" | dt_ <= 4.25) %>%
-
-      # Some steps start at 7 but they take longer than 8 hours.
-      # We remove those steps as well
-      filter(., dt_ < 8.25) %>%
-
-      # Now that we removed some steps we also need to remove some of the turning
-      # angles that we are not able to calculate without them
-      (function(x){
-
-        # Loop through all rows (the first row has an NA anyways)
-        for (i in 2:nrow(x)){
-
-          # Check if the current step starts where the previous step ended
-          if (x$t1_[i] != x$t2_[i-1]){
-
-            # If this is not the case, set the turning angle to NA
-            x$ta_[i] <- NA
-          }
-        }
-        # Return the resulting tibble
-        return(x)
-      })
-  })) %>%
-
-  # Unnest the tibble
-  unnest() %>%
-
-  # Multiply turning angles with negative one (for some reason the package
-  # calculates the turning angles conuterclockwise but we want them clockwise)
-  mutate(ta_ = ta_ * (-1)) %>%
-
-  # Add a column indicating the absolute turning angle (important for the
-  # ellipses). We can use the function we created above.
-  mutate(absta_ = absAngle(.)) %>%
-
-  # We can only work with steps for which we have a turning angle. Let's get rid
-  # of any steps where the turning angle is NA
-  filter(!is.na(ta_)) %>%
-
-  # We also want a unique step id and an indicator that the steps refer to
-  # realized steps
-  mutate(
-      step_id_  = 1:nrow(.)
-    , case_     = 1
-  )
-
-# Store the 4 hourly tracks
-tracks4hours <- tracks
-
-############################################################
-#### Data Cleaning (For 24 Hourly Fixes)
-############################################################
-# For this part we will use the GPS fixes from dispersers only
-tracks <- data %>%
-
-  # Again, we only want to extract the data for dispersers
-  filter(., State == "Disperser") %>%
-
-  # We only want to keep one fix per day, preferrably one that is close to 07:00
-  # Let's be genereous and set a relatively high threshold. Note a tighter
-  # threshold only marginally changes the number of fixes we end up with
-  filter(.
-    , strftime(round_date(Timestamp, "60 minutes")
-      , tz      = "UTC"
-      , format  = "%H:%M:%S") %in%
-      c("03:00:00", "04:00:00", "05:00:00", "06:00:00", "07:00:00", "08:00:00"
-        , "09:00:00", "10:00:00", "11:00:00")) %>%
-
-  # Coerce tibble to dataframe
-  as.data.frame() %>%
-
-  # If there are multiple fixes on a day, we want to keep only the fix that is
-  # closest to 07:00
-  resFix(., hours = 24, start = 7, individual = "DogName") %>%
-
-  # Using the subsetted GPS fixes we prepare tracks
-  make_track(.
-    , .x = x
-    , .y = y
-    , .t = Timestamp
-    , id = DogName
-    , crs = CRS("+init=epsg:4326")
-    , State = State
-  ) %>%
-
-  # Transform the tracks to utm
-  transform_coords(CRS("+init=epsg:32734")) %>%
-
-  # Add a column that indicates the time of the day. Note that we request to get
-  # dusk and dawn as well. I follow Gabrieles paper and night starts when the sun
-  # is 18 degree below the horizion
-  time_of_day(., solar.dep = 18, include.crepuscule = FALSE) %>%
-
-  # Nest the tracks
-  nest(data = -"id") %>%
-
-  # Add column that indicates number of fixes
-  mutate(NoFixes = data %>%
-    lapply(., nrow) %>%
-    do.call(rbind, .) %>%
-    as.vector(.)) %>%
-
-  # Keep only those individuals where we have more than 3 fixes during disp.
-  subset(., NoFixes > 3) %>%
-
-  # Turn to a step representation (look up the amt vignette for details)
-  mutate(data = map(data, function(x){
-    x %>%
-
-      # The function creates steps from the fixes in each tibble row. The option
-      # "keep_cols" allows to keep the time of day (tod) column that we added
-      steps(., keep_cols = "start") %>%
-
-      # Transform the difftime column to a numeric column to avoid that there are
-      # heterogeneous units
-      mutate(., dt_ = as.numeric(dt_, units = "hours")) %>%
-
-      # We get rid of any steps that take longer than 29 hours or shorter than
-      # 20 hours
-      filter(., dt_ <= 32 & dt_ >= 20) %>%
-
-      # Now that we removed some steps we also need to remove some of the turning
-      # angles that we are not able to calculate without them
-      (function(x){
-
-        # Loop through all rows (the first row has an NA anyways)
-        for (i in 2:nrow(x)){
-
-          # Check if the current step starts where the previous step ended
-          if (x$t1_[i] != x$t2_[i-1]){
-
-            # If this is not the case, set the turning angle to NA
-            x$ta_[i] <- NA
-          }
-        }
-        # Return the resulting tibble
-        return(x)
-      })
-  })) %>%
-
-  # Unnest the tibble
-  unnest(.) %>%
-
-  # Multiply turning angles with negative one (for some reason the package
-  # calculates the turning angles conuterclockwise but we want them clockwise)
-  mutate(., ta_ = ta_ * (-1)) %>%
-
-  # Add a column indicating the absolute turning angle (important for the
-  # ellipses). We can use the function we created
-  mutate(., absta_ = absAngle(.)) %>%
-
-  # We can only work with steps for which we have a turning angle. Let's get rid
-  # of any steps where the turning angle is NA
-  filter(., !is.na(ta_)) %>%
-
-  # We also want a unique step id and an indicator that the steps refer to
-  # realized steps
-  mutate(., step_id_ = 1:nrow(.), case_ = 1)
-
-# Store the 24 hourly tracks
-tracks24hours <- tracks
-
-############################################################
 #### Generation of Random Steps
-############################################################
-# Depending on the fixrate schedule the code will look slightly different
-if (selection == 24){
-  tracks <- tracks24hours
-} else {
-  tracks <- tracks4hours
-}
-
-# We could use the amt package to create random steps. However, the package
-# doesn't allow to sample step lengths from the same distribution for all
-# individuals. We will thus write our own code. First of all, we need to make
-# all steps larger than zero. Only if all steps are greater than zero we can fit
-# a gamma distribution. So let's add one meter to all steps that are currently
-# zero (only four steps)
+################################################################################
+# First of all, we need to make all steps larger than zero. Only if all steps
+# are greater than zero we can fit a gamma distribution. So let's add one meter
+# to all steps that are currently zero (only four steps)
 tracks$sl_[tracks$sl_ == 0] <- 1
 
+# We want to normalize the step length to 4 hours (for TiSSF). However, remember
+# that we assume steps between 7 and 15 oclock to be half as quick as the other
+# steps. Let's thus identify the steps that were realized between 7 and 15
+# o'clock
+index <- hour(round_date(tracks$t1_, "1 hour")) >= 7 &
+  hour(round_date(tracks$t2_, "1 hour")) <= 15 &
+  hour(round_date(tracks$t1_, "1 hour")) < hour(round_date(tracks$t2_, "1 hour"))
+
+# Let's also create a column indicating these fixes
+tracks$InactiveTime <- index
+
+# Create a corrected dt
+tracks$dtcorr_ <- tracks$dt_
+tracks$dtcorr_[tracks$InactiveTime] <- tracks$dt_[tracks$InactiveTime] / 2
+
+# Normalize step lengths to 4 hours (only important for TiSSF)
+tracks$normsl_ <- tracks$sl_ / tracks$dtcorr_ * 4
+
 # Fit a Gamma distribution to the observed step lengths
-sl <- fit_distr(tracks$sl_, "gamma")
+if (method == "TiSSF"){
+  sl <- fit_distr(tracks$normsl_, "gamma")
+} else {
+  sl <- fit_distr(tracks$sl_, "gamma")
+}
+
+# Also calculate the rate
+sl$params$rate <- 1 / sl$params$scale
+
+# Let's visualize the fit (note that rate = 1 / scale, i.e. scale = 1 / rate)
+x <- seq(0, max(tracks$sl_), 1)
+y <- dgamma(x, rate = sl$params$rate, shape = sl$params$shape)
+hist(tracks$sl_, freq = F, breaks = 100)
+lines(y ~ x, col = "red")
 
 # Write the distribution to file
-saveRDS(sl, "03_Data/03_Results/99_GammaDistribution.rds")
+if (method == "TiSSF"){
+  write_rds(sl, "03_Data/03_Results/99_GammaDistribution(TiSSF).rds")
+} else {
+  write_rds(sl, "03_Data/03_Results/99_GammaDistribution(iSSF).rds")
+}
 
 # Define the number of random steps
 nsteps <- 24
 
-# Prepare an empty list in which we can store the random steps of each individual
-randomSteps <- list()
-
 # Now we use the fitted gamma distribution to sample step lengths. For the
 # turning angles we follow Avgar et al. 2016 and use a uniform distribution
-for (i in 1:nrow(tracks)){
+randomSteps <- pbmclapply(
+    X                  = 1:nrow(tracks)
+  , mc.cores           = detectCores() - 1
+  , ignore.interactive = T
+  , FUN                = function(x){
 
   # Draw random turning angles
   ta_new <- runif(nsteps, min = -pi, max = pi)
 
-  # Draw random step lengths with the fitted parameters
+  # Draw random step lengths with the fitted parameters. In case we want to
+  # apply TiSSF, we need to adjust the rate parameter of the distribution
+  # accordingly
   sl_new <- rgamma(nsteps
     , shape = sl$params$shape
-    , scale = sl$params$scale)
+    , rate  = sl$params$rate
+  )
 
-  # Put the step lengths and turning angles into a new dataframe and calculate the
-  # new endpoints of each random step. We also indicate that the steps are control
-  # steps (i.e. 0)
-  randomSteps[[i]] <- tracks[rep(i, nsteps), ] %>%
+  if (method == "TiSSF"){
+    sl_new <- sl_new * tracks$dtcorr_[x] / 4
+  }
+
+  # # In case we're using TiSSF, we need to calculate a normalized step length
+  # # (normalized to 4 hours). Again, remember that fixes taken during the
+  # # "inactive" time, need to be halved.
+  # if (method == "TiSSF"){
+  #   if (tracks$InactiveTime[x]){
+  #     sl_new <- sl_new * tracks$dt_[x] / 8
+  #   } else {
+  #     sl_new <- sl_new * tracks$dt_[x] / 4
+  #   }
+  # }
+
+  # Put the step lengths and turning angles into a new dataframe and calculate
+  # the new endpoints of each random step. We also indicate that the steps are
+  # control steps (i.e. 0)
+  randomSteps <- tracks[rep(x, nsteps), ] %>%
     mutate(.
       , absta_  = absta_ + (ta_new - ta_)
       , ta_     = ta_new
@@ -432,10 +231,11 @@ for (i in 1:nrow(tracks)){
       , x2_     = x1_ + sin(absta_) * sl_
       , y2_     = y1_ + cos(absta_) * sl_
     )
-}
 
-# Collapse the list of dataframes into a single dataframe
-randomSteps <- do.call(rbind, randomSteps)
+  # Return the sampled steps
+  return(randomSteps)
+
+}) %>% do.call(rbind, .)
 
 # We need to make sure that the absolute turning angle ranges from 0 to 2 * pi
 randomSteps$absta_[randomSteps$absta_ > 2 * pi] <-
@@ -445,12 +245,7 @@ randomSteps$absta_[randomSteps$absta_ < 0] <-
 
 # Merge the dataframes of the observed and random steps
 ssf <- rbind(tracks, randomSteps) %>%
-
-  # Sort them according to animal id and case/control
   arrange(step_id_, desc(case_)) %>%
-
-  # Make sure that the case/control column is a logical indicator (coerce 1 to
-  # TRUE, 0 to FALSE).
   transform(case_ = as.logical(case_))
 
 # Coerce the steps to spatial lines. We can use the function we defined earlier
@@ -460,15 +255,27 @@ lines <- lineTrack(ssf, CRS("+init=epsg:32734"))
 # Transform the lines to WGS84
 lines <- spTransform(lines, CRS("+init=epsg:4326"))
 
+# # Reproject coordinates
+# tracks[, c("x1_", "y1_")] <- reprojCoords(
+#     xy    = cbind(tracks$x1_, tracks$y1_)
+#   , from  = CRS("+init=epsg:32734")
+#   , to    = CRS("+init=epsg:4326")
+# )
+# tracks[, c("x2_", "y2_")] <- reprojCoords(
+#     xy    = cbind(tracks$x2_, tracks$y2_)
+#   , from  = CRS("+init=epsg:32734")
+#   , to    = CRS("+init=epsg:4326")
+# )
+
 # Coerce the duration column to a format that can be stored (numeric rather than
 # difftime)
 lines$dt_ <- as.numeric(lines$dt_)
 
 # Store the object to a shapefile
-if (selection == 24){
-  filename <- "00_General_Dispersers_Popecol(Random24Hours)"
+if (method == "iSSF"){
+  filename <- "00_General_Dispersers_Popecol(iSSF)"
 } else {
-  filename <- "00_General_Dispersers_Popecol(Random4Hours)"
+  filename <- "00_General_Dispersers_Popecol(TiSSF)"
 }
 
 # Store the stuff into both output directories
@@ -478,6 +285,10 @@ writeOGR(lines
   , driver    = "ESRI Shapefile"
   , overwrite = TRUE
 )
+
+################################################################################
+#### CONTINUE HERE!!!
+################################################################################
 
 ############################################################
 #### Extraction of Covariates: Water
